6be897be4aa5b5bed609439a3187bc8bdd4cfab1
Mohamad Abomaizer <mabomaizer>
2024-07-17 11:17:35 +0300

    Start working on iOS

---
+++ CICD/CICD/CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by Mohammad Abu Maizer on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
